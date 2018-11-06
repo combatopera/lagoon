c5ac37d91fd95b81ff5ea1d573fafda6fac926df
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2018-11-06 21:26:15 +0000

    playing around with an idea

---
+++ example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyven
+
+from system import echo
+
+def main():
+    echo('Hello, world!')
+    echo('Hello,', 'world!')
+    echo('Hello, world!')
+
+if '__main__' == __name__:
+    main()
