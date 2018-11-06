e702e38fda33464887c46bd74dba6b6d3eb6f452
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2018-11-06 21:44:41 +0000

    yay

--- example.py
+++ example.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env pyven
 
-from system import echo
+from system import echo, false
+from subprocess import CalledProcessError
 
 def main():
     echo('Hello, world!')
     echo('Hello,', 'world!')
     echo('Hello, world!')
+    try:
+        false()
+    except CalledProcessError:
+        pass
+    try:
+        from system import thisisnotanexecutable
+    except ImportError:
+        pass
 
 if '__main__' == __name__:
     main()
