6386fc7bead61517b51547b6c9e2403a4c2c6540
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-10-05 11:26:43 +0100

    rename project

--- test_lagoon.py
+++ test_lagoon.py
@@ -1,33 +1,33 @@
 # Copyright 2018, 2019 Andrzej Cichocki
 
-# This file is part of system.
+# This file is part of lagoon.
 #
-# system is free software: you can redistribute it and/or modify
+# lagoon is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# system is distributed in the hope that it will be useful,
+# lagoon is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with system.  If not, see <http://www.gnu.org/licenses/>.
+# along with lagoon.  If not, see <http://www.gnu.org/licenses/>.
 
 from subprocess import CalledProcessError
 import unittest
 
-class TestSystem(unittest.TestCase):
+class TestLagoon(unittest.TestCase):
 
     def test_nosuchprogram(self):
         def imp():
-            from system import thisisnotanexecutable
+            from lagoon import thisisnotanexecutable
             del thisisnotanexecutable
         self.assertRaises(ImportError, imp)
 
     def test_false(self):
-        from system import false
+        from lagoon import false
         false(check = False)
         false(check = None)
         false(check = ())
@@ -35,6 +35,6 @@ class TestSystem(unittest.TestCase):
         self.assertRaises(CalledProcessError, lambda: false(check = 'x'))
 
     def test_works(self):
-        from system import echo
+        from lagoon import echo
         echo.decode = True
         self.assertEqual('Hello, world!\n', echo('Hello,', 'world!').stdout)
--- test_screen.py
+++ test_screen.py
@@ -1,19 +1,19 @@
 # Copyright 2018, 2019 Andrzej Cichocki
 
-# This file is part of system.
+# This file is part of lagoon.
 #
-# system is free software: you can redistribute it and/or modify
+# lagoon is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# system is distributed in the hope that it will be useful,
+# lagoon is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with system.  If not, see <http://www.gnu.org/licenses/>.
+# along with lagoon.  If not, see <http://www.gnu.org/licenses/>.
 
 from screen import Stuff, screenenv
 from pathlib import Path
